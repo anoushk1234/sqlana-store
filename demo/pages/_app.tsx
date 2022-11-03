a03ada8efbd6608ef40ccef4cd388d166f01c5d7
anoushk1234 <anoushka1828@gmail.com>
2022-11-04 01:04:27 +0530

    analytics

--- demo/pages/_app.tsx
+++ demo/pages/_app.tsx
@@ -4,25 +4,34 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WalletKitProvider
-      app={{
-        name: "Sqlana Store",
-        icon: (
-          <img
-            src="https://goki.so/assets/android-chrome-256x256.png"
-            alt="icon"
-          />
-        ),
-      }}
-    >
-      {" "}
-      <Toaster />
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </WalletKitProvider>
+    <>
+      <Script
+        async
+        defer
+        data-website-id="175a63c0-47c0-492c-a585-f94b6aec20e1"
+        src="https://analytics.metapasshq.xyz/umami.js"
+      />
+      <WalletKitProvider
+        app={{
+          name: "Sqlana Store",
+          icon: (
+            <img
+              src="https://goki.so/assets/android-chrome-256x256.png"
+              alt="icon"
+            />
+          ),
+        }}
+      >
+        {" "}
+        <Toaster />
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </WalletKitProvider>
+    </>
   );
 }
 
